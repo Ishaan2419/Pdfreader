88e2fa0ece9202322ffe282cd04ae9cd59dfed84
Ishaan <ishaanraut02@gmail.com>
2026-03-30 21:20:25 +0530

    Update pdfreader.py

--- pdfreader.py
+++ pdfreader.py
@@ -26,3 +26,55 @@ if upload_file is not None:
 
     st.subheader("Dataset Preview")
     st.write("File uploaded successfully")
+
+
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+
+    chunks = text.split("\n")
+
+   
+    embeddings = embed_model.encode(chunks)
+    embeddings = np.array(embeddings).astype("float32")
+
+ 
+    dimension = embeddings.shape[1]
+    index = faiss.IndexFlatL2(dimension)
+    index.add(embeddings)
+
+    st.success("✅ PDF processed successfully!")
+
+    query = st.text_input("Ask a question:")
+
+    if query:
+        # Encode query
+        query_embedding = embed_model.encode([query]).astype("float32")
+
+        # Search
+        k = 2
+        distances, indices = index.search(query_embedding, k)
+
+        retrieved = [chunks[i] for i in indices[0]]
+        context = " ".join(retrieved)
+
+        # Prompt
+        prompt = f"Context:\n{context}\n\nQuestion: {query}\nAnswer:\n"
+
+        # Generate answer
+        result = generator(
+            prompt,
+            max_new_tokens=50,
+            do_sample=False,
+            pad_token_id=50256
+        )
+
+        output = result[0]['generated_text']
+
+        answer = output[len(prompt):]
+        answer = answer.split("Question:")[0]
+        answer = answer.split("Q:")[0]
+
+        # Display
+        st.subheader("🧠 Answer:")
+        st.write(answer.strip())
